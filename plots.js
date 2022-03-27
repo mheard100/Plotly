b67d758c1a5bf77c676000357f653d8a3aabcf3a
mheard100 <97119920+mheard100@users.noreply.github.com>
2022-03-27 14:04:16 -0500

    Update plots.js

--- plots.js
+++ plots.js
@@ -1,37 +1,43 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+// Init function takes ID num and sets them as
+// options of the dropdown menu
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
-
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
+  // optionChanged function is called in html
+  function optionChanged(newSample) {
+    console.log(newSample);
+    buildMetadata(newSample);
+    buildCharts(newSample);
   }
-};
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+  // buildMetadata function called in optionChanged
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultsArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultsArray[0]; //Because results are returned like an array =[0]
+      var PANEL = d3.select("#sample-metadata");
+
+      PANEL.html(""); //Clear content before other id gets called
+      PANEL.append("h6").text(`ID: ${result.id}`);
+      PANEL.append("h6").text(`Ethnicity: ${result.ethnicity}`);
+      PANEL.append("h6").text(`Gender: ${result.gender}`);
+      PANEL.append("h6").text(`Age: ${result.age}`);
+      PANEL.append("h6").text(`Location: ${result.location}`);
+      PANEL.append("h6").text(`BBtype: ${result.bbtype}`);
+      PANEL.append("h6").text(`Washing/week: ${result.wfreq}`);
+    });
+  }
